38d1bb8b21c63e8415ea613df7b4e846da901868
Need-Being <32161782+need-being@users.noreply.github.com>
2020-12-10 23:15:20 +0800

    improve perf

--- sm3/sm3block.go
+++ sm3/sm3block.go
@@ -26,7 +26,6 @@ func p1(x uint32) uint32 { return x ^ bits.RotateLeft32(x, 15) ^ bits.RotateLeft
 // It's used by sm3block_generic.go and tests.
 func blockGeneric(dig *digest, p []byte) {
 	var w [68]uint32
-	var wp [64]uint32
 
 	h0, h1, h2, h3, h4, h5, h6, h7 := dig.h[0], dig.h[1], dig.h[2], dig.h[3], dig.h[4], dig.h[5], dig.h[6], dig.h[7]
 	for len(p) >= chunk {
@@ -36,15 +35,12 @@ func blockGeneric(dig *digest, p []byte) {
 		for i := 16; i < 68; i++ {
 			w[i] = p1(w[i-16]^w[i-9]^bits.RotateLeft32(w[i-3], 15)) ^ bits.RotateLeft32(w[i-13], 7) ^ w[i-6]
 		}
-		for i := 0; i < 64; i++ {
-			wp[i] = w[i] ^ w[i+4]
-		}
 
 		a, b, c, d, e, f, g, h := h0, h1, h2, h3, h4, h5, h6, h7
 		for i := 0; i < 16; i++ {
 			ss1 := bits.RotateLeft32(bits.RotateLeft32(a, 12)+e+bits.RotateLeft32(t0, i), 7)
 			ss2 := ss1 ^ bits.RotateLeft32(a, 12)
-			tt1 := ff0(a, b, c) + d + ss2 + wp[i]
+			tt1 := ff0(a, b, c) + d + ss2 + (w[i] ^ w[i+4])
 			tt2 := gg0(e, f, g) + h + ss1 + w[i]
 			d = c
 			c = bits.RotateLeft32(b, 9)
@@ -58,7 +54,7 @@ func blockGeneric(dig *digest, p []byte) {
 		for i := 16; i < 64; i++ {
 			ss1 := bits.RotateLeft32(bits.RotateLeft32(a, 12)+e+bits.RotateLeft32(t1, i), 7)
 			ss2 := ss1 ^ bits.RotateLeft32(a, 12)
-			tt1 := ff1(a, b, c) + d + ss2 + wp[i]
+			tt1 := ff1(a, b, c) + d + ss2 + (w[i] ^ w[i+4])
 			tt2 := gg1(e, f, g) + h + ss1 + w[i]
 			d = c
 			c = bits.RotateLeft32(b, 9)
